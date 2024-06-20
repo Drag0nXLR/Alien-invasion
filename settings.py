76d07afc84ba7f4d0b97e981d3dd557b7aa59214
Drag0n <kovalyov17vova@gmail.com>
2024-06-20 18:46:10 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,22 @@
+class Settings:
+    """Клас для збереження налаштувань гри."""
+
+    def __init__(self):
+        """Ініціалізувати налаштування гри."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 5
+
+        # Alien settings
+        self.alien_speed = 0.5
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
