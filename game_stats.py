807ee934bf7597182e1225da1b17f8744d146607
Eugene Kovalyov <eugene.kovalyov@itechcraft.com>
2024-06-20 19:17:24 +0300

    Created GameStats class

--- game_stats.py
+++ game_stats.py
@@ -1,2 +1,8 @@
 class GameStats:
-    pass
+
+    def __init__(self, ai_game):
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        self.ships_left = self.settings.ship_limit
